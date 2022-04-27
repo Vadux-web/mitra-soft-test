01a130d7f18cae336a67fbee3ebfa80dfa6f306d
Vadim Lunev <lunev82@gmail.com>
2022-04-27 20:29:04 +0300

    Navbar refactor on NavbarOffcanvas

--- src/components/Navibar/Navibar.jsx
+++ src/components/Navibar/Navibar.jsx
@@ -1,35 +1,39 @@
 import React from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
 import Styles from "./Styles";
+import { Container, Nav, Navbar, Offcanvas } from "react-bootstrap";
 import avatar from "../../assets/images/624301325.png";
 
 let Navibar = () => {
   return (
     <div>
       <Styles>
-        <Navbar
-          fixed="top"
-          collapseOnSelect
-          expand="none"
-          bg="dark"
-          variant="dark"
-        >
+        <Navbar fixed="top" bg="dark" variant="dark" expand={false}>
           <Container>
-            <Navbar.Brand />
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <div className="info">
-                <img className="avatar" src={avatar} alt={"ava"} />
-                <div>
-                  <p className="info_text">Лунёв Вадим</p>
-                  <p className="info_text">lunev82@gmail.com</p>
+            <Navbar.Brand href="#">MitraSoft (test)</Navbar.Brand>
+            <Navbar.Toggle aria-controls="offcanvasNavbar" />
+            <Navbar.Offcanvas
+              id="offcanvasNavbar"
+              aria-labelledby="offcanvasNavbarLabel"
+              placement="start"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id="offcanvasNavbarLabel">
+                  Вадим Лунёв
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <div className="info">
+                  <img className="avatar" src={avatar} alt={"ava"} />
+                  <div>
+                    <p className="info_text">lunev82@gmail.com</p>
+                  </div>
                 </div>
-              </div>
-              <Nav className="me-auto">
-                <Nav.Link href="/">Галерея</Nav.Link>
-                <Nav.Link href="/about">Обо мне</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link href="/">Галерея</Nav.Link>
+                  <Nav.Link href="/about">Обо мне</Nav.Link>
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
           </Container>
         </Navbar>
       </Styles>
